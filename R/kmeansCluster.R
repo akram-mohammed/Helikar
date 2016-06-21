e6a6ea8fc1f0deb0fabe03270784b6da45f04b51
shubham <shubham@shubham>
2016-06-21 22:54:55 +0530

    kmeans data 6

---
+++ R/kmeansCluster.R
@@ -0,0 +1,20 @@
+kmeansCluster <- function(myname = ""){
+
+  library(jsonlite)
+
+  data <- fromJSON(myname)
+  colnames(data) <- c("X", "Y")
+
+  cl <- kmeans(x = data, centers = 6)
+
+  data <- cbind(description = c(1:dim(data)[1]), data, cl = cl$cluster)
+
+  center <- cl$centers
+  colnames(center) <- c("X", "Y")
+  center <- cbind(description = rep("center", dim(center)[1]), center, cl = c(1:dim(center)[1]))
+
+  data <- merge(data, center, all = TRUE)
+
+  json_string <- toJSON(data)
+  list(message = paste(json_string))
+}
