8f438a789b804149b925f020f7ab528b3b42c3d4
shubhamkmr47 <shubhamkmr47@gmail.com>
2016-07-20 00:59:02 +0530

    removed NA from group bar data

--- R/groupbar.R
+++ R/groupbar.R
@@ -10,6 +10,9 @@ groupBar <- function(data = ""){
   data <- sapply(data, as.character)
   data <- as.data.frame(data)
 
+  l <- dim(data)[1]
+  data <- data[1:l-1, ]
+  
   json_string <- toJSON(data)
 
   list(message = paste(json_string))
